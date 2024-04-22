b5ce01c51392120c41cbe49f7d0d85e2ee6793a7
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-04-21 19:42:21 -0700

    Adding test case four

---
+++ ntlang/four_s.s
@@ -0,0 +1,131 @@
+main:
+
+    addi sp, sp, -1024
+
+    # Initializing reg values as 0 or input
+    lbu a0, (x0)
+    lbu a1, (x1)
+    lbu a2, (x2)
+    lbu a3, (x3)
+    lbu a4, (x4)
+    lbu a5, (x5)
+    lbu a6, (x6)
+    lbu a7, (x7)
+    
+    jal codegen_func_s
+    
+    addi sp, sp, 1024
+    
+    unimp
+codegen_func_s:
+four_s:
+	addi sp, sp, -8
+	sd ra, (sp)
+	addi sp, sp, -8
+	sd a0, (sp)
+	addi sp, sp, -8
+	sd a1, (sp)
+	addi sp, sp, -8
+	sd a2, (sp)
+	ld t1, (sp)
+	addi sp, sp, 8
+	ld t0, (sp)
+	addi sp, sp, 8
+	add t0, t0, t1
+	addi sp, sp, -8
+	sd t0, (sp)
+	addi sp, sp, -8
+	sd a3, (sp)
+	ld t1, (sp)
+	addi sp, sp, 8
+	ld t0, (sp)
+	addi sp, sp, 8
+	div t0, t0, t1
+	addi sp, sp, -8
+	sd t0, (sp)
+	ld t1, (sp)
+	addi sp, sp, 8
+	ld t0, (sp)
+	addi sp, sp, 8
+	mul t0, t0, t1
+	addi sp, sp, -8
+	sd t0, (sp)
+	ld t0, (sp)
+	addi sp, sp, 8
+	sub t0, zero, t0
+	addi sp, sp, -8
+	sd t0, (sp)
+	addi sp, sp, -8
+	sd a4, (sp)
+	ld t1, (sp)
+	addi sp, sp, 8
+	ld t0, (sp)
+	addi sp, sp, 8
+	sra t0, t0, t1
+	addi sp, sp, -8
+	sd t0, (sp)
+	addi sp, sp, -8
+	sd a5, (sp)
+	ld t1, (sp)
+	addi sp, sp, 8
+	ld t0, (sp)
+	addi sp, sp, 8
+	sll t0, t0, t1
+	addi sp, sp, -8
+	sd t0, (sp)
+	ld t0, (sp)
+	addi sp, sp, 8
+	li t1, -1
+	xor t0, t0, t1
+	addi sp, sp, -8
+	sd t0, (sp)
+	addi sp, sp, -8
+	sd a6, (sp)
+	ld t1, (sp)
+	addi sp, sp, 8
+	ld t0, (sp)
+	addi sp, sp, 8
+	srl t0, t0, t1
+	addi sp, sp, -8
+	sd t0, (sp)
+	addi sp, sp, -8
+	li t0, 14
+	sd t0, (sp)
+	ld t1, (sp)
+	addi sp, sp, 8
+	ld t0, (sp)
+	addi sp, sp, 8
+	xor t0, t0, t1
+	addi sp, sp, -8
+	sd t0, (sp)
+	addi sp, sp, -8
+	li t0, 30
+	sd t0, (sp)
+	ld t1, (sp)
+	addi sp, sp, 8
+	ld t0, (sp)
+	addi sp, sp, 8
+	and t0, t0, t1
+	addi sp, sp, -8
+	sd t0, (sp)
+	addi sp, sp, -8
+	li t0, 16
+	sd t0, (sp)
+	ld t0, (sp)
+	addi sp, sp, 8
+	li t1, -1
+	xor t0, t0, t1
+	addi sp, sp, -8
+	sd t0, (sp)
+	ld t1, (sp)
+	addi sp, sp, 8
+	ld t0, (sp)
+	addi sp, sp, 8
+	or t0, t0, t1
+	addi sp, sp, -8
+	sd t0, (sp)
+	ld a0, (sp)
+	addi sp, sp, 8
+	ld ra, (sp)
+	addi sp, sp, 8
+	ret
